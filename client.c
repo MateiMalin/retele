d162732efb1d1087e5607296e5963f624a60f4f4
mateimalin <mateimalin08@gmail.com>
2025-12-08 01:58:52 +0200

    Initial commit

---
+++ client.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <time.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+
+extern int errno;
+
+int port;
+char my_id[32];
+float current_lat = 0.0;
+float current_long = 0.0;
+int current_speed = 0;
+int is_initialized = 0;
+
+void show_prompt()
+{
+    printf("\r[%s] Comanda > ", my_id);
+    fflush(stdout);
+}
+
+// argumentele o sa fie domeniu, port si id-ul vehiculului
+int main(int argc, char *argv[])
+{
+    int sd;                         // descriptorul de socket
+    struct sockaddr_in server;      // structura folosita pentru conectare
+    char buffer[BUFFER_SIZE];       // mesajul trimis
+    char input_buffer[BUFFER_SIZE]; // mesaju de la tastatura
+
+    fd_set readfds;
+    struct timeval tv;
+    int ret;
+    /* exista toate argumentele in linia de comanda? */
+    if (argc != 4)
+    {
+        printf("[client] Sintaxa: %s <adresa_server> <port> <id-masina>\n", argv[0]);
+        return -1;
+    }
+
+    /* stabilim portul */
+    port = atoi(argv[2]);
+    strcpy(my_id, argv[3]);
+    srand(time(NULL));
+
+    /* cream socketul */
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("[client] Eroare la socket().\n");
+        return errno;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+    server.sin_port = htons(port);
+
+    /* ne conectam la server */
+    if (connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
+    {
+        perror("[client]Eroare la connect().\n");
+        return errno;
+    }
+
+    // trimitem imediat connect la server ca sa inregistram o masina
+    snprintf(buffer, sizeof(buffer), "{\"cmd\":\"CONNECT\", \"id\":\"%s\"}", my_id);
+    write(sd, buffer, strlen(buffer));
+
+    // --- INTERFATA UTILIZATOR (Simulata in Terminal) ---
+    printf("\n=================================================\n");
+    printf("   TRAFFIC MONITOR CLIENT - %s\n", my_id);
+    printf("=================================================\n");
+    printf("1. Initializare:  set <lat> <long> <viteza>\n");
+    printf("   Exemplu:       set 47.16 27.58 60\n");
+    printf("2. Raportare:     report\n");
+    printf("3. Abonare:       sub / unsub\n");
+    printf("4. Iesire:        exit\n");
+    printf("=================================================\n");
+    printf("Nota: Dupa 'set', datele se trimit automat la fiecare 5 secunde.\n\n");
+
+    show_prompt();
+
+    // accept doar accepta o conexiune cand esti conectat cu un file descriptor
+    while (1)
+    {
+        FD_ZERO(&readfds);
+        FD_SET(0, &readfds);  // Monitorizam Tastatura (stdin)
+        FD_SET(sd, &readfds); // Monitorizam Serverul (socket)
+
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+
+        ret = select(sd + 1, &readfds, NULL, NULL, &tv);
+        // select e ala care o sa si modifice readfds
+        // ret e return value de la select si imi spune in ce stadiu sunt
+        if (ret == -1)
+        {
+            perror("Eroare la select");
+            break;
+        }
+
+        if (ret == 0)
+        {
+            // inseamna ca au trecut alea 60 de secunde si trebuie sa trimit telemetry din nou
+            if (is_initialized)
+            {
+                current_speed += (rand() % 5) - 2;
+                if (current_speed < 0)
+                    current_speed = 0;
+                current_lat += 0.1;
+                current_long += 0.1;
+
+                snprintf(buffer, sizeof(buffer), "{\"cmd\":\"TELEMETRY\", \"lat\":\"%.4f\", \"long\":\"%.4f\", \"speed\":\"%d\", \"id\":\"%s\"}", current_lat, current_long, current_speed, my_id);
+
+                write(sd, buffer, strlen(buffer));
+            }
+            continue;
+        }
+
+        // daca primim de la server mesaje(alerte sau confirmari)
+        // verificam daca socket-ul e inca on
+        if (FD_ISSET(sd, &readfds))
+        {
+            memset(buffer, 0, sizeof(buffer));
+            int bytes = read(sd, buffer, sizeof(buffer) - 1);
+            buffer[bytes] = '\0';
+
+            if (bytes <= 0)
+            {
+                printf("\n[SISTEM] Serverul s-a inchis. Iesire...\n");
+                break;
+            }
+
+            if (strstr(buffer, "ALERT") && strstr(buffer, "Viteza prea mare"))
+            {
+                int new_limit = 0;
+
+                char *p = strstr(buffer, "limita de");
+                if (p)
+                {
+                    sscanf(p + 10, "%d", &new_limit);
+
+                    if (new_limit > 0 && current_speed > new_limit)
+                    {
+                        current_speed = new_limit;
+                        printf("\n[AUTO-PILOT] Viteza redusa automat la %d km/h conform alertei.\n", current_speed);
+                    }
+                }
+            }
+            else
+            {
+                printf("\n[Server ACK]: %s\n", buffer);
+            }
+            show_prompt();
+        }
+
+        if (FD_ISSET(0, &readfds))
+        {
+            // verificam daca citim ceva de la tastatura
+            fgets(input_buffer, sizeof(input_buffer), stdin);
+            char *aux = strchr(input_buffer, '\n');
+            if (aux)
+                *aux = '\0';
+
+            if (strcmp(input_buffer, "exit") == 0)
+            {
+                break;
+            }
+            else if (strncmp(input_buffer, "set", 3) == 0)
+            {
+                // avem comanda de set, deci trebuie sa mai citim de la tastatura si parametrii
+                if (sscanf(input_buffer, "set %f %f %d", &current_lat, &current_long, &current_speed) == 3)
+                {
+                    is_initialized = 1;
+
+                    printf("[CLIENT] Locatie setata. Pornire telemetrie...\n");
+
+                    snprintf(buffer, sizeof(buffer), "{\"cmd\":\"TELEMETRY\", \"lat\":\"%.4f\", \"long\":\"%.4f\", \"speed\":\"%d\", \"id\":\"%s\"}", current_lat, current_long, current_speed, my_id);
+                    write(sd, buffer, strlen(buffer));
+                }
+                else
+                {
+                    printf("[EROARE] Format gresit. Foloseste: set 47.16 27.58 60\n");
+                }
+            }
+            else if (strncmp(input_buffer, "report", 6) == 0)
+            {
+                // scriem id-ul masiniim, ca sa ne luam dupa asta pentru locatia strazii
+                snprintf(buffer, sizeof(buffer), "{\"cmd\":\"REPORT\", \"type\":\"ACCIDENT\", \"id\":\"%s\"}", my_id);
+                write(sd, buffer, strlen(buffer));
+            }
+            else if (strcmp(input_buffer, "sub") == 0)
+            {
+                snprintf(buffer, sizeof(buffer), "{\"cmd\":\"SUBSCRIBE\", \"id\":\"%s\"}", my_id);
+                write(sd, buffer, strlen(buffer));
+            }
+            else if (strcmp(input_buffer, "unsub") == 0)
+            {
+                snprintf(buffer, sizeof(buffer), "{\"cmd\":\"UNSUBSCRIBE\", \"id\":\"%s\"}", my_id);
+                write(sd, buffer, strlen(buffer));
+            }
+            else
+            {
+                printf("[CLIENT] Comanda necunoscuta.\n");
+            }
+            show_prompt();
+        }
+    }
+
+    close(sd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,442 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
+#include <time.h>
+
+#define PORT 2728
+#define MAX_CLIENTS 100
+
+extern int errno;
+
+pthread_mutex_t data_lock = PTHREAD_MUTEX_INITIALIZER;
+
+int max_long = -2e9;
+int max_lat = -2e9;
+// struct for the car info
+struct client_info
+{
+    char id[32];
+    int active;           // if the car is active
+    int subscription;     // if it has an ongoing subscription for weather etc etc
+    float lat;            // coord strada
+    float lng;            // coord strada
+    char street_name[64]; // nume strada
+    int speed;            // the speed
+} clients[MAX_CLIENTS];
+
+typedef struct
+{
+    // datele statice din json
+    int id;
+    char name[64];
+    float lat_min, lat_max;
+    float long_min, long_max;
+    int default_speed_limit;
+
+    // datele dinamice
+    int current_speed_limit;
+    int has_accident;
+    int traffic_level;
+    int car_count;
+
+} RoadSegment;
+
+RoadSegment city_map[100];
+int map_size = 100;
+
+// functie care sa returneze indexul unei strazi sau -1 daca
+// masina nu se afla pe o strada
+int find_street_index(float lat, float lng)
+{
+    for (int i = 0; i < map_size; i++)
+    {
+        if (city_map[i].id == 0)
+            continue;
+
+        if (lat >= city_map[i].lat_min && lat <= city_map[i].lat_max &&
+            lng >= city_map[i].long_min && lng <= city_map[i].long_max)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// functie care sa alerteze masinile daca trebuie sa reduca viteza
+void alert_speed(int s_index, int new_speed_limit, int alert_case, int sender_fd)
+{
+    char alert_msg[512];
+    char s_name[63];
+
+    if (s_index != -1)
+    {
+        strcpy(s_name, city_map[s_index].name);
+    }
+    else
+    {
+        strcpy(s_name, "Strada nu exista");
+    }
+
+    if (alert_case == 1 || alert_case == 2)
+    {
+        // s-a petrecut accident pe strada cu s_index sau e trafic
+        if (s_index != -1)
+            city_map[s_index].current_speed_limit = new_speed_limit;
+
+        char *reason;
+        if (alert_case == 1)
+            reason = "ACCIDENT GRAV";
+        else
+            reason = "TRAFIC CRESCUT";
+
+        snprintf(alert_msg, sizeof(alert_msg), "{\"cmd\":\"ALERT\", \"type\":\"CRITIC\", \"msg\":\"%s pe %s. Noua limita: %d km/h\"}\n", reason, s_name, new_speed_limit);
+
+        // ii lueam pe toti la rand
+        for (int fd = 0; fd < MAX_CLIENTS; fd++)
+        {
+            if (clients[fd].active)
+                if (strcmp(clients[fd].street_name, s_name) == 0)
+                    write(fd, alert_msg, strlen(alert_msg));
+        }
+    }
+    else if (alert_case == 3)
+    {
+        // doar conduce cu prea multa viteza
+        snprintf(alert_msg, sizeof(alert_msg), "{\"cmd\":\"ALERT\", \"type\":\"WARNING\", \"msg\":\"Atentie %s! Ati depasit limita de %d km/h!\"}\n",
+                 clients[sender_fd].id, new_speed_limit);
+        write(sender_fd, alert_msg, strlen(alert_msg));
+    }
+}
+
+// Functia Thread-ului (Inlocuieste handle_client)
+void *client_thread(void *arg)
+{
+    int fd = *((int *)arg);
+    free(arg);
+
+    char buffer[1024];
+    char response[1024];
+
+    while (1)
+    {
+        // read este blocant in thread-uri, ceea ce e ok
+        int bytes = read(fd, buffer, sizeof(buffer) - 1);
+
+        if (bytes <= 0)
+        {
+            printf("Clientul cu fd : %d s-a deconectat\n", fd);
+
+            // Protejam stergerea din lista globala
+            pthread_mutex_lock(&data_lock);
+
+            int old_street = find_street_index(clients[fd].lat, clients[fd].lng);
+            if (old_street != -1 && city_map[old_street].car_count > 0)
+                city_map[old_street].car_count--;
+
+            clients[fd].active = 0;
+            pthread_mutex_unlock(&data_lock);
+
+            close(fd);
+            break;
+        }
+
+        buffer[bytes] = '\0';
+
+        if (strstr(buffer, "CONNECT"))
+        {
+            char *ptr = strstr(buffer, "\"id\":\"");
+            if (ptr)
+            {
+                char id[32];
+                sscanf(ptr + 6, "%[^\"]", id);
+
+                // Protejam scrierea datelor noului client
+                pthread_mutex_lock(&data_lock);
+                strcpy(clients[fd].id, id);
+                clients[fd].active = 1;
+                clients[fd].lat = 0;
+                clients[fd].lng = 0;
+                clients[fd].speed = 0;
+                clients[fd].subscription = 0;
+                strcpy(clients[fd].street_name, "Unknown");
+                pthread_mutex_unlock(&data_lock);
+
+                printf("[CONNECT] S-a conectat un client nou: %s (socket fd: %d)\n", id, fd);
+                snprintf(response, sizeof(response), "{\"status\":\"OK\", \"msg\":\"Welcome %s\"}\n", id);
+                write(fd, response, strlen(response));
+            }
+        }
+        else if (strstr(buffer, "UNSUBSCRIBE"))
+        {
+            pthread_mutex_lock(&data_lock);
+            clients[fd].subscription = 0;
+            pthread_mutex_unlock(&data_lock);
+
+            printf("[UNSUBSCRIBE] Clientul cu id-ul: %s s-a dezabonat\n", clients[fd].id);
+            snprintf(response, sizeof(response), "{\"status\":\"OK\", \"msg\":\"Unsubscribed\"}\n");
+            write(fd, response, strlen(response));
+        }
+        else if (strstr(buffer, "SUBSCRIBE"))
+        {
+            pthread_mutex_lock(&data_lock);
+            clients[fd].subscription = 1;
+            pthread_mutex_unlock(&data_lock);
+
+            printf("[SUBSCRIBE] Clientul cu id-ul: %s s-a abonat\n", clients[fd].id);
+            snprintf(response, sizeof(response), "{\"status\":\"OK\", \"msg\":\"Subscribed\"}\n");
+            write(fd, response, strlen(response));
+        }
+        else if (strstr(buffer, "TELEMETRY"))
+        {
+            float temp_lat = 0;
+            float temp_long = 0;
+            int temp_speed = 0;
+            char *p;
+
+            p = strstr(buffer, "\"lat\":\"");
+            if (p)
+                sscanf(p + 7, "%f", &temp_lat);
+
+            p = strstr(buffer, "\"long\":\"");
+            if (p)
+                sscanf(p + 8, "%f", &temp_long);
+
+            p = strstr(buffer, "\"speed\":\"");
+            if (p)
+                sscanf(p + 9, "%d", &temp_speed);
+
+            pthread_mutex_lock(&data_lock);
+
+            // intainte sa dam udpate, vedem care era indexul strazii vechi
+            int old_index = find_street_index(clients[fd].lat, clients[fd].lng);
+            int street_index = find_street_index(temp_lat, temp_long);
+
+            if (old_index != -1 && old_index != street_index)
+            {
+                if (city_map[old_index].car_count > 0)
+                    city_map[old_index].car_count--;
+            }
+
+            clients[fd].lat = temp_lat;
+            clients[fd].lng = temp_long;
+            clients[fd].speed = temp_speed;
+
+            printf("[UPDATE] Clientul %s: lat: %.4f, long: %.4f, speed: %d\n",
+                   clients[fd].id, temp_lat, temp_long, temp_speed);
+
+            if (street_index != -1)
+            {
+                strcpy(clients[fd].street_name, city_map[street_index].name);
+
+                if (street_index != old_index)
+                {
+                    // inseamna ca am schimbat strada pe care ne aflam
+                    city_map[street_index].car_count++;
+                }
+
+                snprintf(response, sizeof(response),
+                         "Va aflati pe strada: %s , conduceti cu %d km/h iar limita de viteza este de: %d\n",
+                         city_map[street_index].name,
+                         clients[fd].speed,
+                         city_map[street_index].current_speed_limit);
+                write(fd, response, strlen(response));
+
+                if (city_map[street_index].has_accident)
+                {
+                    alert_speed(street_index, 10, 1, fd);
+                    snprintf(response, sizeof(response), "{\"cmd\":\"ALERT\", \"msg\":\"Accident pe %s! Limita 10 km/h\"}\n", city_map[street_index].name);
+                    write(fd, response, strlen(response));
+                }
+
+                if (city_map[street_index].car_count > 5)
+                {
+                    city_map[street_index].traffic_level = 3;
+                    city_map[street_index].current_speed_limit = 30;
+
+                    alert_speed(street_index, 30, 2, fd);
+                    // snprintf(response, sizeof(response), "{\"cmd\":\"ALERT\", \"msg\":\"Trafic RIDICAT pe %s! Limita 30km/h\"}", city_map[street_index].name);
+                    // write(fd, response, strlen(response));
+                }
+                else if (city_map[street_index].car_count > 3)
+                {
+                    city_map[street_index].traffic_level = 2;
+                    city_map[street_index].current_speed_limit = 40;
+
+                    alert_speed(street_index, 40, 2, fd);
+                    // snprintf(response, sizeof(response), "{\"cmd\":\"ALERT\", \"msg\":\"Trafic MEDIU pe %s! Limita 40km/h\"}", city_map[street_index].name);
+                    // write(fd, response, strlen(response));
+                }
+
+                if (clients[fd].speed > city_map[street_index].current_speed_limit)
+                {
+                    alert_speed(street_index, city_map[street_index].current_speed_limit, 3, fd);
+                    snprintf(response, sizeof(response), "{\"cmd\":\"ALERT\", \"msg\":\"Viteza prea mare! Limita este %d km/h\"}\n", city_map[street_index].current_speed_limit);
+                    write(fd, response, strlen(response));
+                }
+            }
+            else
+            {
+                strcpy(clients[fd].street_name, "Unknown");
+                snprintf(response, sizeof(response), "Alert, the street name is not known...please try something else...\n");
+                write(fd, response, strlen(response));
+            }
+
+            pthread_mutex_unlock(&data_lock);
+        }
+        else if (strstr(buffer, "REPORT"))
+        {
+            printf("[REPORT] Clientul %s raporteaza: accident\n", clients[fd].id);
+
+            int street_idx = find_street_index(clients[fd].lat, clients[fd].lng);
+
+            if (street_idx != -1)
+            {
+                // Setam accidentul protejat
+                pthread_mutex_lock(&data_lock);
+                city_map[street_idx].has_accident = 1;
+                pthread_mutex_unlock(&data_lock);
+
+                // Alert speed are propriul lock intern, deci e safe sa il apelam fara lock aici
+                alert_speed(street_idx, 10, 1, fd);
+
+                snprintf(response, sizeof(response), "{\"status\":\"RECEIVED\", \"msg\":\"Raport accident inregistrat pe %s. Multumim!\"}\n", city_map[street_idx].name);
+            }
+            else
+            {
+                snprintf(response, sizeof(response), "{\"status\":\"ERROR\", \"msg\":\"Nu te putem localiza pe o strada cunoscuta.\"}\n");
+            }
+            write(fd, response, strlen(response));
+        }
+    }
+    return NULL;
+}
+
+void load_map_from_json()
+{
+    FILE *f = fopen("streets.json", "r");
+    if (!f)
+    {
+        printf("[WARNING] streets.json lipseste. Folosesc harta goala.\n");
+        return;
+    }
+
+    char line[512];
+    map_size = -1;
+    while (fgets(line, sizeof(line), f))
+    {
+        if (strstr(line, "{"))
+        {
+            map_size++;
+            city_map[map_size].has_accident = 0;
+            city_map[map_size].car_count = 0;
+        }
+        if (strstr(line, "\"id\":"))
+            sscanf(strstr(line, ":") + 1, "%d", &city_map[map_size].id);
+        if (strstr(line, "\"name\":"))
+        {
+            char *start = strchr(line, ':');
+            if (start)
+                sscanf(start + 2, "\"%[^\"]\"", city_map[map_size].name);
+        }
+        if (strstr(line, "\"lat_min\":"))
+            sscanf(strstr(line, ":") + 1, "%f", &city_map[map_size].lat_min);
+        if (strstr(line, "\"lat_max\":"))
+            sscanf(strstr(line, ":") + 1, "%f", &city_map[map_size].lat_max);
+        if (strstr(line, "\"lon_min\":"))
+            sscanf(strstr(line, ":") + 1, "%f", &city_map[map_size].long_min);
+        if (strstr(line, "\"lon_max\":"))
+            sscanf(strstr(line, ":") + 1, "%f", &city_map[map_size].long_max);
+        if (strstr(line, "\"speed_limit\":"))
+        {
+            sscanf(strstr(line, ":") + 1, "%d", &city_map[map_size].default_speed_limit);
+            city_map[map_size].current_speed_limit = city_map[map_size].default_speed_limit;
+        }
+
+        if (city_map[map_size].lat_max > max_lat)
+            max_lat = city_map[map_size].lat_max;
+        if (city_map[map_size].long_max > max_long)
+            max_long = city_map[map_size].long_max;
+    }
+    map_size++;
+    fclose(f);
+    printf("[server] Harta incarcata: %d strazi.\n", map_size);
+}
+
+int main()
+{
+    load_map_from_json();
+    struct sockaddr_in server;
+    struct sockaddr_in from;
+    int sd;
+    int optval = 1;
+    socklen_t len = sizeof(from);
+
+    // Initializare
+    memset(clients, 0, sizeof(clients));
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("[server] Eroare la creare socket");
+        return errno;
+    }
+
+    setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    memset(&server, 0, sizeof(server));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
+    {
+        perror("[server] Eroare la bind");
+        return errno;
+    }
+
+    if (listen(sd, 10) == -1)
+    {
+        perror("[server] Eroare la listen");
+        return errno;
+    }
+
+    printf("[server] (Multithreaded) Asteptam la portul %d...\n", PORT);
+    fflush(stdout);
+
+    while (1)
+    {
+        int client_sock = accept(sd, (struct sockaddr *)&from, &len);
+
+        if (client_sock < 0)
+        {
+            perror("[server] Eroare la accept");
+            continue;
+        }
+
+        printf("[server] Client nou conectat: %d. Lansez thread...\n", client_sock);
+
+        // thread safety,alocam pe heap memorie
+        int *new_sock = malloc(sizeof(int));
+        *new_sock = client_sock;
+
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, client_thread, (void *)new_sock) < 0)
+        {
+            perror("Eroare la creare thread");
+            free(new_sock);
+            close(client_sock);
+        }
+        else
+        {
+            // Detasam thread-ul ca sa isi elibereze singur resursele la final
+            pthread_detach(tid);
+        }
+    }
+    return 0;
+}
